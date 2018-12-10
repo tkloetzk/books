8b0dc532d29382ef354a0cfd2e74eb49f4d2a9fe
Tucker Kloetzke <tuckerkloetzke@gmail.com>
2018-12-09 21:23:09 -0600

    Scripts

--- routes/api/amazon.js
+++ routes/api/amazon.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const rp = require("request-promise");
-const get = require("lodash").get;
-const resultParse = require("../../nodejs/amazon/resultParse");
+const express = require('express');
+const rp = require('request-promise');
+const get = require('lodash').get;
+const resultParse = require('../../nodejs/amazon/resultParse');
 
-const url = "https://www.amazon.com/s/rstripbooks&ie=UTF8&keywords=";
+const url = 'https://www.amazon.com/s/rstripbooks&ie=UTF8&keywords=';
 
 const router = express.Router();
 
-router.post("/v1", (req, res) => {
-  const isbnArray = get(req.body, "isbns").split(","); // use lodash
+router.post('/v1', (req, res) => {
+  const isbnArray = get(req.body, 'isbns').split(','); // use lodash
   console.log(isbnArray);
   rp(url)
     .then(html =>
