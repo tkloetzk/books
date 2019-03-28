4901224b06ae1d881334da9b9a300388401fd368
Tucker Kloetzke <tuckerkloetzke@gmail.com>
2019-03-28 16:16:25 -0500

    formatting

--- client/src/store/goodreads/__tests__/goodreadsActions.test.js
+++ client/src/store/goodreads/__tests__/goodreadsActions.test.js
@@ -2,12 +2,12 @@ import * as actions from '../goodreadsActions';
 import * as types from '../goodreadsActionTypes';
 import { LOADING_STATUSES } from '../../../util/constants';
 import { getGoodreadsSingleBooksService } from '../../../services/goodreadsService';
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
-const createMockStore = configureMockStore([thunk])
+const createMockStore = configureMockStore([thunk]);
 
-jest.mock('../../../services/goodreadsService')
+jest.mock('../../../services/goodreadsService');
 
 describe('goodreads actions', () => {
   let book;
@@ -17,7 +17,7 @@ describe('goodreads actions', () => {
       goodreadsAverageRating: 3.5,
       goodreadsRatingsCount: 90,
     };
-    getGoodreadsSingleBooksService.mockClear()
+    getGoodreadsSingleBooksService.mockClear();
   });
   it('can set goodreads book hasErrored', () => {
     const actionTrue = actions.getGoodreadsBookFailure(true);
@@ -31,7 +31,7 @@ describe('goodreads actions', () => {
       type: types.FETCH_GOODREADS_BOOK_HAS_ERRORED,
       hasErrored: false,
     });
-  })
+  });
   it('can set goodreads book isLoading', () => {
     const action = actions.getGoodreadsBookIsLoading(LOADING_STATUSES.initial);
     expect(action).toEqual({
@@ -47,32 +47,47 @@ describe('goodreads actions', () => {
     });
   });
   it('can clear goodreads books', () => {
-    const dispatch = jest.fn()
-    actions.clearGoodreadsBooks()(dispatch)
-    expect(dispatch.mock.calls).toMatchSnapshot()
-  })
+    const dispatch = jest.fn();
+    actions.clearGoodreadsBooks()(dispatch);
+    expect(dispatch.mock.calls).toMatchSnapshot();
+  });
   it('can clear goodreads books success', () => {
-    const action = actions.clearGoodreadsBooksSuccess()
+    const action = actions.clearGoodreadsBooksSuccess();
     expect(action).toEqual({
       type: types.CLEAR_GOODREADS_BOOKS_SUCCESS,
-    })
-  })
+    });
+  });
   it('can get google book', () => {
-   const store = createMockStore()
-   const expectedResponse = [{"isLoading": "loading", "type": "FETCH_GOODREADS_BOOK_IS_LOADING"}, {"isLoading": "success", "type": "FETCH_GOODREADS_BOOK_IS_LOADING"}, {"book": {"goodreadsAverageRating": 3.5, "goodreadsRatingsCount": 90, "isbn": "9781400079094"}, "type": "FETCH_GOODREADS_BOOK_SUCCESS"}]
-   getGoodreadsSingleBooksService.mockResolvedValue(book)
+    const store = createMockStore();
+    const expectedResponse = [
+      { isLoading: 'loading', type: 'FETCH_GOODREADS_BOOK_IS_LOADING' },
+      { isLoading: 'success', type: 'FETCH_GOODREADS_BOOK_IS_LOADING' },
+      {
+        book: {
+          goodreadsAverageRating: 3.5,
+          goodreadsRatingsCount: 90,
+          isbn: '9781400079094',
+        },
+        type: 'FETCH_GOODREADS_BOOK_SUCCESS',
+      },
+    ];
+    getGoodreadsSingleBooksService.mockResolvedValue(book);
 
     return store.dispatch(actions.getGoodreadsBook(book.isbn)).then(() => {
-      expect(store.getActions()).toEqual(expectedResponse)
-    })
-  })
+      expect(store.getActions()).toEqual(expectedResponse);
+    });
+  });
   it('can throw error on get goodreads book', () => {
-    const store = createMockStore()
-    const expectedResponse = [{"isLoading": "loading", "type": "FETCH_GOODREADS_BOOK_IS_LOADING"}, {"isLoading": "errored", "type": "FETCH_GOODREADS_BOOK_IS_LOADING"}, {"hasErrored": true, "type": "FETCH_GOODREADS_BOOK_HAS_ERRORED"}]
-    getGoodreadsSingleBooksService.mockRejectedValue(null)
+    const store = createMockStore();
+    const expectedResponse = [
+      { isLoading: 'loading', type: 'FETCH_GOODREADS_BOOK_IS_LOADING' },
+      { isLoading: 'errored', type: 'FETCH_GOODREADS_BOOK_IS_LOADING' },
+      { hasErrored: true, type: 'FETCH_GOODREADS_BOOK_HAS_ERRORED' },
+    ];
+    getGoodreadsSingleBooksService.mockRejectedValue(null);
 
     return store.dispatch(actions.getGoodreadsBook(book.isbn)).then(() => {
-      expect(store.getActions()).toEqual(expectedResponse)
-    })
-  })
-})
+      expect(store.getActions()).toEqual(expectedResponse);
+    });
+  });
+});
--- client/src/store/google/__tests__/googleReducer.test.js
+++ client/src/store/google/__tests__/googleReducer.test.js
@@ -19,12 +19,12 @@ describe('google reducer', () => {
   it('should return inital state if type is not found and no state passed in', () => {
     const testState = googleReducer(undefined, { type: 'FAKE' });
     expect(testState).toEqual(initialState);
-  })
+  });
   it('should return given state if type is not found', () => {
     const expectedState = 'example state';
     const resultState = googleReducer(expectedState, { type: 'FAKE' });
     expect(resultState).toBe(expectedState);
-  })
+  });
   describe('FETCH_GOOGLE_BOOK_SUCCESS', () => {
     it('can save book to empty books array', () => {
       const resultState = googleReducer(initialState, {
@@ -32,82 +32,102 @@ describe('google reducer', () => {
         book,
       });
       expect(resultState.books).toEqual([book]);
-    })
-  
+    });
+
     it('can save book to an exisiting books array', () => {
       const books = [
         {
           title: 'Existing book',
           isbn: '12341234',
-          subtitle: "Subtitle",
+          subtitle: 'Subtitle',
           description: 'Descritipn',
           thumbnail:
             'http://books.google.com/books/coover&img=1&zoom=1&edge=curl&source=gbs_api',
           categories: ['Test Cat'],
-        }
-      ]
+        },
+      ];
       const state = Object.assign({}, initialState, { books });
       const resultState = googleReducer(state, {
         type: types.FETCH_GOOGLE_BOOK_SUCCESS,
         book,
       });
       expect(resultState.books).toEqual([...books, book]);
-    })
-  })
+    });
+  });
   describe('FETCH_GOOGLE_BOOK_HAS_ERRORED', () => {
     it('can set hasErrored state to true', () => {
-      const hasErroredState = googleReducer({}, { 
-        type: types.FETCH_GOOGLE_BOOK_HAS_ERRORED,
-        hasErrored: true });
+      const hasErroredState = googleReducer(
+        {},
+        {
+          type: types.FETCH_GOOGLE_BOOK_HAS_ERRORED,
+          hasErrored: true,
+        }
+      );
       expect(hasErroredState.hasErrored).toEqual(true);
-    })
+    });
 
     it('can set hasErrored state to true', () => {
-      const hasErroredState = googleReducer({}, { 
-        type: types.FETCH_GOOGLE_BOOK_HAS_ERRORED,
-        hasErrored: false });
+      const hasErroredState = googleReducer(
+        {},
+        {
+          type: types.FETCH_GOOGLE_BOOK_HAS_ERRORED,
+          hasErrored: false,
+        }
+      );
       expect(hasErroredState.hasErrored).toEqual(false);
-    })
-  })
+    });
+  });
 
   describe('FETCH_GOOGLE_BOOK_IS_LOADING', () => {
-    it('can set isLoading state to loading' , () => {
-      const loadingState = googleReducer({}, { 
-        type: types.FETCH_GOOGLE_BOOK_IS_LOADING,
-        isLoading: LOADING_STATUSES.loading });
+    it('can set isLoading state to loading', () => {
+      const loadingState = googleReducer(
+        {},
+        {
+          type: types.FETCH_GOOGLE_BOOK_IS_LOADING,
+          isLoading: LOADING_STATUSES.loading,
+        }
+      );
       expect(loadingState.isLoading).toEqual(LOADING_STATUSES.loading);
-    })
+    });
 
-    it('can set isLoading state to success' , () => {
-      const loadingState = googleReducer({}, { 
-        type: types.FETCH_GOOGLE_BOOK_IS_LOADING,
-        isLoading: LOADING_STATUSES.success });
+    it('can set isLoading state to success', () => {
+      const loadingState = googleReducer(
+        {},
+        {
+          type: types.FETCH_GOOGLE_BOOK_IS_LOADING,
+          isLoading: LOADING_STATUSES.success,
+        }
+      );
       expect(loadingState.isLoading).toEqual(LOADING_STATUSES.success);
-    })
+    });
 
-    it('can set isLoading state to errored' , () => {
-      const loadingState = googleReducer({}, { 
-        type: types.FETCH_GOOGLE_BOOK_IS_LOADING,
-        isLoading: LOADING_STATUSES.errored });
+    it('can set isLoading state to errored', () => {
+      const loadingState = googleReducer(
+        {},
+        {
+          type: types.FETCH_GOOGLE_BOOK_IS_LOADING,
+          isLoading: LOADING_STATUSES.errored,
+        }
+      );
       expect(loadingState.isLoading).toEqual(LOADING_STATUSES.errored);
-    })
-  })
+    });
+  });
   describe('CLEAR_GOOGLE_BOOKS_SUCCESS', () => {
     const books = [
       {
         title: 'Existing book',
         isbn: '12341234',
-        subtitle: "Subtitle",
+        subtitle: 'Subtitle',
         description: 'Descritipn',
         thumbnail:
           'http://books.google.com/books/coover&img=1&zoom=1&edge=curl&source=gbs_api',
         categories: ['Test Cat'],
-      }
-    ]
+      },
+    ];
     const state = Object.assign({}, initialState, { books });
     const resultState = googleReducer(state, {
       type: types.CLEAR_GOOGLE_BOOKS_SUCCESS,
     });
     expect(resultState).toEqual(initialState);
-  })
-})
+  });
+});
