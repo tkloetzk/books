ab35a3c7f03bb5c820ec1a47b55bbc0d8cb1c816
tkloetzk <tuckerkloetzke@gmail.com>
2019-04-09 22:30:29 -0500

    Refresh bookshelf 2 (#16)
    * Dev (#14) (#15)
    
    * Stage configs
    
    * switch to npm
    
    * updates

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,17 +1,17 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api/amazon/v1', { target: 'http://localhost:5000/' }));
-  app.use(proxy('/api/amazon/v2', { target: 'http://localhost:5000/' }));
+  app.use(proxy('/api/amazon/v1', { target: 'http://127.0.0.1:5000/' }));
+  app.use(proxy('/api/amazon/v2', { target: 'http://127.0.0.1:5000/' }));
   app.use(proxy('/api/bookshelf', { target: 'http://localhost:5000/' }));
-  if (process.env.REACT_APP_ENV === 'Offline') {
+  if (process.env.REACT_APP_ENV !== 'Offline') { //TODO: This doesn't seem right
   app.use(proxy('/api/goodreads/v1', { target: 'http://localhost:8080/' }));
 } else {
   app.use(proxy('/api/goodreads/v1', { target: 'http://localhost:5000/' }));
 }
   app.use(
     proxy('/api/google/v1', {
-      target: 'http://localhost:5000/',
+      target: 'http://127.0.0.1:5000/',
     })
   );
 };
