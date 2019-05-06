86beb763669222656ad61bdb34007f83fd645621
Tucker Kloetzke <tuckerkloetzke@gmail.com>
2019-05-06 06:12:41 -0500

    minor updates

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const amazon = require('./routes/api/amazon');
-const amazonOffline = require('./routes/api/offline/amazonOffline')
+const amazonOffline = require('./routes/api/offline/amazonOffline');
 const goodreads = require('./routes/api/goodreads');
 const goodreadsOffline = require('./routes/api/offline/goodreadsOffline');
 const bookshelf = require('./routes/api/bookshelf');
 const bookshelfOffline = require('./routes/api/offline/bookshelfOffline');
 const google = require('./routes/api/google');
 const googleOffline = require('./routes/api/offline/googleOffline');
-const spawn = require('child_process').spawn
+const spawn = require('child_process').spawn;
 
 const app = express();
 // configure app to use bodyParser()
@@ -24,13 +24,13 @@ var env = process.env.NODE_ENV || 'dev';
 let db;
 let dbName;
 if (env === 'dev') {
-  dbName = 'Using database Test'
+  dbName = 'Using database Test';
   db = keys.mongoURITest;
 } else if (env === 'stg') {
-  dbName = 'Using database Stage'
+  dbName = 'Using database Stage';
   db = keys.mongoURIStage;
 } else if (env === 'prod') {
-  dbName = 'Using database Prod'
+  dbName = 'Using database Prod';
   db = keys.mongoURI;
 }
 
@@ -39,7 +39,7 @@ if (env === 'offline') {
   app.use('/api/bookshelf', bookshelfOffline);
   app.use('/api/amazon', amazonOffline);
   app.use('/api/google', googleOffline);
-  app.use("/api/goodreads", goodreadsOffline);
+  app.use('/api/goodreads', goodreadsOffline);
 } else {
   console.info(`not offline. using ${dbName} bookshelf`);
   mongoose
@@ -52,22 +52,25 @@ if (env === 'offline') {
   app.use('/api/google', google);
 }
 
-
 const port = process.env.PORT || 5000;
 
-const server = app.listen(port, () => console.info(`Server running on port ${port}`));
+const server = app.listen(port, () =>
+  console.info(`Server running on port ${port}`)
+);
 
-process.on('exit', function() { process.exit(1); });
+process.on('exit', function() {
+  process.exit(1);
+});
 
-process.on( 'SIGTERM', function () {
-   server.close(function () {
-     if (env !== 'offline') {
-       mongoose.connection.close(() => {
-         console.log('close MongoDB connection')
-       })
-     }
-     console.log("Finished all requests");
-   });
+process.on('SIGTERM', function() {
+  server.close(function() {
+    if (env !== 'offline') {
+      mongoose.connection.close(() => {
+        console.log('close MongoDB connection');
+      });
+    }
+    console.log('Finished all requests');
+  });
 });
 
 //On uncaughtException, restart server
