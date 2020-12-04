3819bbb96b0c0444be88a4c513ab815041033d1c
Tucker Kloetzke <tuckerkloetzke@gmail.com>
2020-12-03 20:53:13 -0600

    Migrate to atlas

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
-  mongoURI: 'mongodb://tucker:tanner22@ds127842.mlab.com:27842/bookshelf',
-  mongoURIStage: 'mongodb://tucker:tanner22@ds015740.mlab.com:15740/bookshelf_prod_backup',
+  //mongoURI: 'mongodb://tucker:tanner22@ds127842.mlab.com:27842/bookshelf',
+  mongoURI: 'mongodb+srv://tucker:tanner22@bookshelf.8rqhn.mongodb.net/bookshelf?retryWrites=true&w=majority',
+//  mongoURIStage: 'mongodb://tucker:tanner22@ds015740.mlab.com:15740/bookshelf_prod_backup',
   mongoURITest:
-    'mongodb://tucker:tanner22@ds161345.mlab.com:61345/bookshelf_test',
+    'mongodb+srv://tucker:tanner22@bookshelf-test.ftw6y.mongodb.net/bookshelf_test?retryWrites=true&w=majority',
 };
